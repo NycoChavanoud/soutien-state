3b7ba453b184d021d288bdf2a1ee5eb248d2b4e4
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-04 12:12:41 +0200

    adjust for the exercice starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [result, setresult] = useState("");
+
+  const resetResult = () => {
+    setresult("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setresult(`NOM : ${name}  / MAIL : ${mail}`);
+    setMail("");
+    setName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="containerForm">
+        <form onSubmit={handleSubmit}>
+          <div className="inputWithLabel">
+            <label htmlFor="name">name</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="inputWithLabel">
+            <label htmlFor="mail">Email</label>
+            <input
+              type="email"
+              id="mail"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+            />
+          </div>
+          <button type="submit" id="send">
+            ENVOYER
+          </button>
+        </form>
+        <button onClick={resetResult} id="resetBtn">
+          RESET
+        </button>
+      </div>
+
+      {!result ? (
+        <span></span>
+      ) : (
+        <span className="resultcontainer">{result}</span>
+      )}
+    </>
   );
 }
 
